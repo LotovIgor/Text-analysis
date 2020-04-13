bc99ec4e0ebeb504cfc4b47928a3b6dd821b5227
LotovIgor <ilotov@ngs.ru>
2020-04-13 18:07:38 +0700

    PEP 8

--- name.py
+++ name.py
@@ -1,4 +1,6 @@
 from textblob import *
+
+
 def count_sents(text):
     simb = '.!?'
     cnt = 0
@@ -19,7 +21,7 @@ def count_syll(text):
     return cnt
 
 
-def sent (text):
+def sent(text):
     ru_blob = TextBlob(text.lower())
     lang = ru_blob.detect_language()
     en_blob = ru_blob.translate(from_lang=lang, to='en')
@@ -38,16 +40,18 @@ def main():
     print('количество слогов:', cnt_sy)
     print('средняя длина предложения в словах:', cnt_w/cnt_s)
     print('средняя длина слова в слогах:', cnt_sy/cnt_w)
-    Fl = 206.835 - (1.3 * cnt_w/cnt_s) - (cnt_sy/cnt_w * 60.1)
-    print('индекс удобочитаемости Флеша:', Fl)
-    if Fl > 80:
+    fl = 206.835 - (1.3 * cnt_w / cnt_s) - (cnt_sy / cnt_w * 60.1)
+    print('индекс удобочитаемости Флеша:', fl)
+    if fl > 80:
         print('Текст очень легко читается')
-    elif Fl > 50:
+    elif fl > 50:
         print('Простой текст')
-    elif Fl > 25:
+    elif fl > 25:
         print('Текст немного трудно читать')
     else:
         print('Текст трудно читается')
     sent(text)
+
+
 if __name__ == '__main__':
     main()
